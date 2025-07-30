e7bdc1c4d645a30d1ce9102eb919eabcb1661b7b
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-07-31 00:16:36 +0530

    Add 0271A_BeautifulYear.cpp

---
+++ 800_Level/0271A_BeautifulYear.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool checkDistinctYear(int year) {
+    vector<bool> seen(10, false);
+    while(year>0) {
+        int digit = year%10;
+        if(seen[digit]) return false;
+        seen[digit] = true;
+        year/=10;
+    }
+    return true;
+}
+
+int main() {
+    int y;
+    cin>>y;
+    y++;
+
+    while(true) {
+        if(checkDistinctYear(y)) {
+            cout<<y<<endl;
+            break;
+        }
+        y++;
+    }
+
+    return 0;
+}
