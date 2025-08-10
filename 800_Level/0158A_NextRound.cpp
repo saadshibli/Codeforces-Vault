0d2421ca44c7dce0d6b6d6f89791ac62d51a9ad3
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-08-10 22:48:18 +0530

    Add Q.158A Next Round

--- 800_Level/0158A_NextRound.cpp
+++ 800_Level/0158A_NextRound.cpp
@@ -1,23 +1,6 @@
 // Problem: Next Round
 // Source: https://codeforces.com/problemset/problem/158/A
-//
-// There are n participants in a contest, each with a score (non-increasing order).
-// The k-th place score is arr[k-1]. A participant advances if their score is at least the k-th place score and is positive.
-//
-// Input:
-//   - n, k: number of participants and cutoff place
-//   - n integers: scores of participants (non-increasing order)
-//
-// Output:
-//   - Number of participants who advance to the next round.
-//
-// Example:
-// Input:
-//   8 5
-//   10 9 8 7 7 7 5 5
-// Output:
-//   6
-//
+
 #include<iostream>
 using namespace std;
 
